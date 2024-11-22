5b8aaeb8d5cafff81e78f10ed4c52212b6b9c9c2
JacobOller <164708752+JacobOller@users.noreply.github.com>
2024-11-22 15:26:02 -0500

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -15,30 +15,24 @@ squares = {}
 
 
 def main():
-    window = main_grid()
-
-    
+    window = main_grid() 
     return window
-
-
-def movement():
-    time.sleep(snake_speed)
-    movement()
 
 
 def main_grid():
     window = tk.Tk()
     x = 0
     timer = 1000 # for updating the label, first label updates 1 second after window initially opens.
+    every_20 = 10
 
     for current_row in range(11): # Maybe the current row
-        for current_label in range(21): # Maybe the current column
+        for current_label in range(20): # Maybe the current column
             x += 1
             frame = tk.Frame(
                 master=window,
                 relief=tk.SUNKEN,
                 borderwidth=1
-            )
+            ) # 53
             frame.grid(row=current_row, column=current_label)
             label = tk.Label(master=frame, text=f'{current_row}\n{current_label}',
                               width=6, height=3, fg ='white',bg ='black') # creates each black label
@@ -46,8 +40,11 @@ def main_grid():
             squares[f"key{x}"] = label
 
     for current_label in squares.keys(): # Iterates through each label from the squares dictionary
-        window.after(timer, update_label, squares[current_label])  # updates label from the function (update_label)
-        timer += 1000  # 1 second timer between each update (will probably need to make this faster, maybe .5 sec?)
+        if every_20 % 20 == 0:
+            window.after(timer, update_label, squares[current_label])  # updates label from the function (update_label)
+            timer += 300  # .3 second timer between each update (Can easily be adjusted faster/slower)
+        every_20 += 1
+
     return window
 
 
@@ -55,9 +52,12 @@ def update_label(label):
     label.config(text=f'{label}', width=6, height=3, fg ='black',bg ='red') # This is the actual updating of each label
     return label
 
+def movement():
+    
+
 
 if __name__ == '__main__':
     snake_pos = snake_spawn
     window = main()
     window.mainloop()
-    print(squares)
+    print(type(squares))
