84bd4f3820c45d76b173c5205fa53d1b891cf69f
ztaylor06 <ztaylor4@uvm.edu>
2024-12-02 16:19:05 -0500

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -1,4 +1,5 @@
 
+
 """
 Zach Taylor
 Jacob Ollerhead
@@ -12,9 +13,8 @@ Snake
 import tkinter as tk
 import time
 
-snake_speed = 0.1
-snake_spawn = (1, 10)
-snake_pos = []
+
+direction = []
 squares = {}
 
 
@@ -46,21 +46,25 @@ def main_grid():
             lst = [label, current_row, current_label]
             squares[f"key{x}"] = lst
             
-    starting_square = [5, 2]            
-    update_label(lst, window, starting_square)
+            
+    global direction        
+    direction = [5, 2]            
+    update_label(lst, window)
     
     return window
 
-def update_label(lst, window, next_square):
+def update_label(lst, window):
+    window.bind('<Key>', lambda event: update_direction(event, lst, window))
+    
     for values in squares.values():
-        if values[1] == next_square[0] and values[2] == next_square[1]:
+        if values[1] == direction[0] and values[2] == direction[1]:
+            #print(direction)
             label = values[0]
             lst = values[0], values[1], values[2]
-    window.bind('<Key>', lambda event: update_direction(event, lst, window))
+            label.config(text='', width=6, height=3, fg='green', bg='green')
+            window.after(300, update_label, lst, window)  
             
             
-    label.config(text='', width=6, height=3, fg='green', bg='green')
-    window.after(300, delete_square, label, lst, window, next_square)
 
 
     return label
@@ -69,46 +73,19 @@ def delete_square(label, lst, window, next_square):
     label.config(text=f'', width=6, height=3, fg='black', bg='black')
     
 def update_direction(event, lst, window):
-    direction = [0, 0]
-    print(lst)
-    
+    global direction
+
     if event.char == 'd':
-            #key_pressed = 'd'
-            direction = lst[1], lst[2] + 1
-            print(direction)
-            window.after(300, iteration, direction, lst, window)
+        direction = lst[1], lst[2] + 1
     if event.char == 'a':
         direction = lst[1], lst[2] - 1
     if event.char == 'w':
         direction = lst[1] - 1, lst[2]
     if event.char == 's':
         direction = lst[1] + 1, lst[2]
-    
-    update_label(lst, window, direction)
-    # need to add milisecond wait for this function
 
-
-def iteration(direction, lst, window):
-        direction = direction[0], direction[1] + 1
-        print(direction)
-        update_label(lst, window, direction)
-        window.after(300, iteration, direction, lst, window)
-
-    
-    # if key_pressed == 'a':
-    #     direction = lst[1], lst[2] - 1
-    #     print(direction)
-    #     return direction
-    # if key_pressed == 'w':
-    #     direction = lst[1] - 1, lst[2]
-    #     print(direction)
-    #     return direction
-    # if key_pressed == 's':
-    #     direction = lst[1] + 1, lst[2]
-    #     print(direction)
-    #     return direction
-   
 
 if __name__ == '__main__':
     window = main()
     window.mainloop()
+
