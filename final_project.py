3fb78d5685a6798e8b2087976d7ace9883f33723
JacobOller <164708752+JacobOller@users.noreply.github.com>
2024-12-04 23:32:26 -0500

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -6,120 +6,170 @@ Snake
 """
 
 #need to use current_row and current_column stored in squares[] to save position of snake
-
-
 import tkinter as tk
 import time
 import random
 
 
-direction = []
 squares = {}
-current_key = None
+coordinates_lst = []
 position = [5, 2]
 length = 5
-apples = 3
-former_key = None
+apples = 1
+timer = 300
 number_of_rows = 13
 number_of_columns = 17
-coordinates_lst = []
-list_of_characters_in_size = []
-settings_accessed = None
-timer = 300
+current_key = None
+former_key = None
+difficulty = 'medium'
+dimensions = '13x17'
 
-''' Initial Screen: Includes button to play and button to change settings'''
 
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    screen_x = (screen_width // 2) - (width // 2)
+    screen_y = (screen_height // 2) - (height // 2)
+    window.geometry(f'{width}x{height}+{screen_x}+{screen_y}')
+    return
+
+'''Includes button to play and button to change settings'''
 def initial_screen():
-    starting_window = tk.Tk()
-    starting_window.geometry('500x500')
-    starting_window.title("Starting Window")
+    global difficulty_label
+    global dimensions_label
+    global apples_label
 
-    tk.Label(master=starting_window,
-             text='Default Settings:').pack()
-    tk.Label(master=starting_window,
-             text='Difficulty: Medium\nGrid Size: 13x16\nApples: 3').pack()
+    starting_window = tk.Tk()
+    center_window(starting_window, 600, 500)
+
+    starting_window.title("Starting Window")
+    starting_window.config(bg='#1fa6a8')
+
+    tk.Label(master=starting_window, # Title
+             text='SNAKE',
+             font=('Castellar', 50, 'bold'),
+             width=10,
+             padx=10,
+             pady=10,
+             bg='blue').pack(pady=(0,10))
+    tk.Label(master=starting_window, # Current Settings
+             text='Current Settings:',
+             font=('Unispace', 22),
+             padx=10,
+             pady=10,
+             bg='#1fa6a8').pack()
+    difficulty_label = tk.Label(master=starting_window,
+             text=f'Difficulty: {difficulty}',
+             font=('Unispace', 14),
+             bg='#1fa6a8')
+    difficulty_label.pack()
+    dimensions_label = tk.Label(master=starting_window,
+            text=f'Grid Size: {dimensions}',
+            font=('Unispace', 14),
+            bg='#1fa6a8')
+    dimensions_label.pack()
+    apples_label = tk.Label(master=starting_window,
+            text=f'Apples: {apples}',
+            font=('Unispace', 14),
+            bg='#1fa6a8')
+    apples_label.pack()
     tk.Button(master=starting_window,
               text='Change Settings',
-              command=settings).pack()
+              font=('Unispace', 17),
+              command=settings).pack(pady=(50,0))
     tk.Button(master=starting_window,
               text='Play',
-              command=initial_play).pack()
+              font=('Unispace', 30),
+              bg='blue',
+              command=initial_play).pack(pady=(15,0))
     return starting_window
 
 
-''' Settings: Includes many buttons that are used to change individual settings;
+'''Includes many buttons that are used to change individual settings;
  includes multiple smaller functions'''
-
 def settings():
-    global settings_accessed
-    settings_accessed = True
     settings_window = tk.Tk()
 
     tk.Button(master=settings_window,
                             text="Easy Mode",
-                            command=lambda: mode('easy'),
+                            command=lambda: mode('easy', difficulty_label),
+                            #command=(mode,'easy'),
                             bg='green').pack()
     tk.Button(master=settings_window,
                             text="Medium Mode",
-                            command=lambda: mode('medium'),
+                            command=lambda: mode('medium', difficulty_label),
+                            #command=(mode,'medium'),
                             bg='yellow').pack()
     tk.Button(master=settings_window,
                             text="Hard Mode",
-                            command=lambda: mode('hard'),
+                            command=lambda: mode('hard', difficulty_label),
+                            #command=(mode,'hard'),
                             bg='red').pack()
     tk.Button(master=settings_window,
               text='10x10 Grid',
-              command=lambda: grid_sizer('10x10')).pack()
+              command=lambda: grid_sizer('10x10', dimensions_label)).pack()
     tk.Button(master=settings_window,
               text='10x12 Grid',
-              command=lambda: grid_sizer('10x12')).pack()
+              command=lambda: grid_sizer('10x12', dimensions_label)).pack()
     tk.Button(master=settings_window,
               text='12x12 Grid',
-              command=lambda: grid_sizer('12x12')).pack()
+              command=lambda: grid_sizer('12x12', dimensions_label)).pack()
     tk.Button(master=settings_window,
               text='10x14 Grid',
-              command=lambda: grid_sizer('10x14')).pack()
+              command=lambda: grid_sizer('10x14', dimensions_label)).pack()
     tk.Button(master=settings_window,
               text='13x16 Grid',
-              command=lambda: grid_sizer('13x16')).pack()
+              command=lambda: grid_sizer('13x16', dimensions_label)).pack()
     tk.Button(master=settings_window,
               text='12x18 Grid',
-              command=lambda: grid_sizer('12x18')).pack()
+              command=lambda: grid_sizer('12x18', dimensions_label)).pack()
     tk.Button(master=settings_window,
               text='1 Apple',
-              command=lambda: apple_amount('1 apple')).pack()
+              command=lambda: apple_amount('1 apple', apples_label)).pack()
     tk.Button(master=settings_window,
               text='2 Apples',
-              command=lambda: apple_amount('2 apple')).pack()
+              command=lambda: apple_amount('2 apple', apples_label)).pack()
     tk.Button(master=settings_window,
               text='3 Apples',
-              command=lambda: apple_amount('3 apple')).pack()
+              command=lambda: apple_amount('3 apple', apples_label)).pack()
     tk.Button(master=settings_window,
               text='4 Apples',
-              command=lambda: apple_amount('4 apple')).pack()
+              command=lambda: apple_amount('4 apple', apples_label)).pack()
     tk.Button(master=settings_window,
               text='5 Apples',
-              command=lambda: apple_amount('5 apple')).pack()
+              command=lambda: apple_amount('5 apple', apples_label)).pack()
     tk.Button(master=settings_window,
               text='Exit Settings',
               command=lambda: exit_settings(settings_window)).pack()
 
     return settings_window
 
-def mode(difficulty):
+
+'''Part of Settings'''
+def mode(selected_diff, label):
     global timer
-    if difficulty == 'easy':
+    global difficulty
+    if difficulty == None:
+        return 'medium'
+    if selected_diff == 'easy':
         timer = 400 # number of miliseconds between each snake movement
-    if difficulty == 'medium':
+    if selected_diff == 'medium':
         timer = 300 # number of miliseconds between each snake movement
-    if difficulty == 'hard':
+    if selected_diff == 'hard':
         timer = 200 # number of miliseconds between each snake movement
+    difficulty = selected_diff
+    difficulty_label_update(label)
 
 
-def grid_sizer(selected_size):
-    global number_of_rows
-    global number_of_columns
-    
+def difficulty_label_update(label):
+    label.config(text=f'Difficulty: {difficulty}')
+
+
+'''Part of Settings'''
+def grid_sizer(selected_size, label):
+    list_of_characters_in_size = []
+    global dimensions
+
     for i in selected_size:
         list_of_characters_in_size.append(i)
     number_of_rows = list_of_characters_in_size[0] + list_of_characters_in_size[1] # Takes the first 2 numbers (ex. 10 in 10x12)
@@ -127,13 +177,26 @@ def grid_sizer(selected_size):
     number_of_columns = list_of_characters_in_size[3] + list_of_characters_in_size[4] # Takes the last 2 numbers ( ex. 12 in 10x12 )
     number_of_columns = int(number_of_columns)
 
+    dimensions = f'{number_of_rows}x{number_of_columns}'
+    dimensions_label_update(label)
 
-def apple_amount(amount):
-    print('initiated')
+
+def dimensions_label_update(label):
+    label.config(text=f'Grid Size: {dimensions}')
+
+
+'''Part of Settings'''
+def apple_amount(amount, label):
     global apples
     for i in range(6):
         if amount == f'{i} apple':
             apples = i
+    apples_label_update(label)
+
+
+def apples_label_update(label):
+    label.config(text=f'Apples: {apples}')
+
 
 '''Function that exits the settings'''
 def exit_settings(settings_window):
@@ -146,12 +209,15 @@ def initial_play():
 '''Main Function; First Creates Labels in a grid style (grid is based off of user input from settings), then every 300ms checks for a new keyboard
 input with the check_for_new_key function; Calls update_label function to update each label acording
 to the last key that has been pressed - This is tracked within the update_label function aswell as the 
-check_for_new_key function; creates apples function (number of apples is based off of user input from settings), '''
+check_for_new_key function; creates apples function (number of apples is based off of user input from settings).'''
 def main():
     global window
-    #starting_window.destroy()
-    window = tk.Tk()
+    global x
     x = 0
+
+    window = tk.Tk()
+    center_window(window, number_of_columns*50,number_of_rows*53)
+    temp_window()
 
     for current_row in range(number_of_rows): # Maybe the current row
         for current_column in range(number_of_columns): # Maybe the current column
@@ -162,15 +228,10 @@ def main():
                 borderwidth=1
             )
             frame.grid(row=current_row, column=current_column)
-            # if x % 2 == 0:
-            #     label = tk.Label(master=frame, text='',
-            #                     width=6, height=3, fg ='white',bg ='#47e322') # creates each light green label
-            # else:
-            #     label = tk.Label(master=frame, text='',
-            #                     width=6, height=3, fg ='white',bg ='#1e8106') # creates each dark green label
             label = tk.Label(master=frame, text='',
-                            width=6, height=3, fg ='white',bg ='black') # creates each black background label
+                            width=6, height=3, fg ='white',bg ='#47e322') # creates each light green label
             label.pack()
+
             lst = [label, current_row, current_column]
             squares[f"key{x}"] = lst
     window.after(300, check_for_new_key)
@@ -179,13 +240,18 @@ def main():
     for apple in range(apples): #spawns in apples randomly based off of user input
         while True:
             apple_label = squares[f'key{random.randint(1, number_of_rows * number_of_columns)}'][0]     
-            if apple_label.cget("bg") != 'green' and apple_label.cget("bg") != 'red':
+            if apple_label.cget("bg") != 'blue' and apple_label.cget("bg") != 'red':
                 break
         apple_label.config(text='', width=6, height=3, fg='red', bg='red')
     
     return window
 
 
+'''Called within main; physically updates label based off of keyboard input by calling on_key_press function;
+checks if snake runs into itself - if it does then initiates death screen; creates the apples and checks if
+ snake gets an apple - if it does then adds 1 to the length of the snake through creating a new
+ blue label; calls delete_label function to maintain
+ the correct length of the snake constantly; '''
 def update_label(position):
     global window
     global timer
@@ -201,30 +267,36 @@ def update_label(position):
             y_coordinate = values[2]
             lst = label, x_coordinate, y_coordinate # once above if statement finds correct coordinates, it sets value of lst = label, x, y
             
-            if label.cget("bg") == 'green': #snake dies because it hits itself
+            if label.cget("bg") == 'blue': #snake dies because it hits itself
                 print('death')
                 window.destroy()
             elif label.cget("bg") == 'red': #add length when snake gets an apple
                 length += 1
                 while True:
                     apple_label = squares[f'key{random.randint(1, number_of_rows * number_of_columns)}'][0]     
-                    if apple_label.cget("bg") != 'green' and apple_label.cget("bg") != 'red':
+                    if apple_label.cget("bg") != 'blue' and apple_label.cget("bg") != 'red':
                         break
                 apple_label.config(text='', width=6, height=3, fg='red', bg='red')
-                label.config(text='', width=6, height=3, fg='green', bg='green')
+                label.config(text='', width=6, height=3, fg='blue', bg='blue')
                 window.after(timer * length, delete_label, label) # we can use the after and a timer to delay the deletion of a square making the snake longer
-            elif label.cget("bg") == 'black':
-                label.config(text='', width=6, height=3, fg='green', bg='green')
+            elif label.cget("bg") == '#47e322':
+                label.config(text='', width=6, height=3, fg='blue', bg='blue')
                 window.after(timer * length, delete_label, label) # we can use the after and a timer to delay the deletion of a square making the snake longer
 
 
+'''Function that is called from update_label everytime a new key is pressed and sets current_key = the key'''
 def on_key_press(event):
     global current_key
     current_key = event.keysym
     # bind function returns none, so we made current_key a global variable instead of returning it
 
+
+'''Function that is called from main, is constantly checking for a new keyboard input update; when it recieves a new
+key, checks whether the new key is the opposite key of the former key (a is opposite of d, s is opposite of w) in
+order to ensure that the snake moves correctly; calls update_label to update the future labels to move in the correct direction
+until a new key is pressed, then the direction will change based off of that key.
+'''
 def check_for_new_key():
-    #global current_key
     global position
     global former_key
 
@@ -268,14 +340,45 @@ def check_for_new_key():
     window.after(timer, check_for_new_key) # Runs itself over and over constantly based on the difficulty that the user chose.
 
 
+'''Function that ensures that the length of the snake is correct by constantly changing the color of labels
+from the color of the snake to the background based off of the snake's movement'''
 def delete_label(label):
-    label.config(text='', width=6, height=3, fg='black', bg='black')
+    #label.config(text='', width=6, height=3, fg='black', bg='black')
+    # if label.cget("bg") == '#47e322':
+    label.config(text='', width=6, height=3, fg='#47e322', bg='#47e322')
+    # else:
+    #     label.config(text='', width=6, height=3, fg='#1e8106', bg='#1e8106')
 
 
+def temp_window():
+    temp_win = tk.Tk()
+    temp_win.config(bg='#d058f0')
+    center_window(temp_win, 100, 50)
+    temp_win.focus()
+    temp_win.attributes('-topmost', True)
+    tk.Label(master=temp_win,
+            text='Press a key to start\n(WASD)',
+            bg='#d058f0',
+            font=('Unispace', 6),).pack(expand=True)
+    destroy_temp(temp_win)
+    
+
+def destroy_temp(temp_win):
+    if current_key == 'w' or current_key == 'a' or current_key == 's' or current_key == 'd':
+        temp_win.destroy()
+        return
+    temp_win.after(100, destroy_temp, temp_win)
+
+
+'''Function that creates the death window; is initiated from update_label'''
 def death_window():
     death_window = tk.Tk()
+    death_window.config(bg='#d058f0')
+    center_window(death_window, 100, 50)
     tk.Label(master=death_window,
-             text=f'You died!\nScore of {length - 5}').pack()
+            text=f'You died!\nScore of {length - 5}',
+            bg='#d058f0',
+            font=('Unispace', 8),).pack(expand=True)
     return death_window
 
 
